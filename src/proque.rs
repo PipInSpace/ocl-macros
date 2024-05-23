53e100ff5a608c77c90b966c7ae7f74282f3f0c9
PipInSpace <jupster18@gmx.de>
2024-05-23 19:00:15 +0200

    remove old macro

--- src/proque.rs
+++ src/proque.rs
@@ -13,22 +13,6 @@ macro_rules! proque {
 }
 
 #[macro_export]
-/// Builds a kernel with named arguments from a ProQue and the kernel name. Adds named arguments given as tuples of ("name", arg).
-///
-/// Syntax: `(proque: ProQue, name: &str, $(args: (&str, T)),*)`
-macro_rules! pq_kernel_n {
-    ($pq:expr, $name:expr, $( $arg:expr),*) => {
-        {
-            let mut kernel_builder = $pq.kernel_builder($name);
-            $(
-                kernel_builder.arg_named($arg.0, $arg.1);
-            )*
-            kernel_builder.build().unwrap()
-        }
-    };
-}
-
-#[macro_export]
 /// Builds a kernel with arguments from a ProQue and the kernel name. Adds unnamed arguments or named arguments given as tuples of ("name", arg).
 ///
 /// Syntax: `(proque: ProQue, name: &str, $(args: T),*)`
